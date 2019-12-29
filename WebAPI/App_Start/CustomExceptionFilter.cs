677b92ef132e0571ed98fdbd8397ec98a05f1d38
Noor Care <50613234+NoorCare@users.noreply.github.com>
2019-12-29 14:31:00 +0530

    CustomExceptionFilter (#10)
    Added Custom Exception Filter
    
    Co-authored-by: addongroup <43293229+addongroup@users.noreply.github.com>

---
+++ WebAPI/App_Start/CustomExceptionFilter.cs
@@ -0,0 +1,50 @@
+﻿using Serilog;
+using Serilog.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+namespace WebAPI.App_Start
+{
+    public class CustomExceptionFilter: ExceptionFilterAttribute
+    {      
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            string exceptionMessage = string.Empty;
+
+            if (actionExecutedContext.Exception.InnerException == null)
+            {
+                exceptionMessage = actionExecutedContext.Exception.Message;
+            }
+            else
+            {
+                exceptionMessage = actionExecutedContext.Exception.InnerException.Message;
+            }
+
+            string errorFileLogPath = ConfigurationManager.AppSettings.Get("errorFileLogPath");
+
+            var log  = new LoggerConfiguration()
+                 .WriteTo.File(@errorFileLogPath, Serilog.Events.LogEventLevel.Error)
+                      //.WriteTo.File(@"F:\NC-29-Sept\NoorCare.Api-master\NoorCare.Api\WebAPI\logger.txt", Serilog.Events.LogEventLevel.Error)
+                      // .WriteTo.File(Server.MapPath(ConfigurationManager.AppSettings["logFile"].ToString()), Serilog.Events.LogEventLevel.Error)
+                      //.WriteTo.RollingFile(@"Warnings_{Date}.txt", DateTime.Now.ToString())
+                      .CreateLogger();
+            
+            log.Write(Serilog.Events.LogEventLevel.Error, actionExecutedContext.Exception,"json");
+
+            //We can log this exception message to the file or database.  
+            var response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+            {
+                Content = new StringContent("An unhandled exception was thrown by service."),  
+                    ReasonPhrase = "Internal Server Error.Please Contact your Administrator."
+            };
+            actionExecutedContext.Response = response;
+        }
+    }
+}
