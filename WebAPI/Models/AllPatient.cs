75260a87ff732571ee2753875ce02b246c026640
noorcare <adviananya@gmail.com>
2020-02-24 21:03:59 +0530

    Add Patient Model
    Add Patient Model

---
+++ WebAPI/Models/AllPatient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class AllPatient
+    {
+        public List<Patient> Patients { get; set; }
+
+    }
+    public class Patient
+    {
+        public string Name { get; set; }
+        public string ClientId { get; set; }
+        public string appointmentId { get; set; }
+        public string LastVisit { get; set; }
+        public string LastVisitDate { get; set; }
+        public string LastVisitTime { get; set; }
+        public string TotalVisit { get; set; }
+    }
+}
