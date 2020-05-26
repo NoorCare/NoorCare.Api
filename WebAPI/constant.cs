a33e6c304c9f57e8d05343e7564ff4afd921e79f
Noor Care <NoorCareNew@gmail.com>
2020-05-26 23:07:30 +0530

    Update constant.cs

--- WebAPI/constant.cs
+++ WebAPI/constant.cs
@@ -10,13 +10,13 @@ namespace WebAPI
         //public static string clientBaseUrl = "http://noorcare.net/";
         //public static string emailidVerefactionUrl = $"{clientBaseUrl}user/EmailVerfication/CLIENTID";
         //public static string logoUrl = $"{clientBaseUrl}assets/images/logos/Logo.png";
-        //public static string apiBaseUrl = "http://api.noorcare.net/";
-        //public static string imgUrl = $"{apiBaseUrl}ProfilePic";
+        //public static string baseUrl = "http://api.noorcare.net/";
+        //public static string imgUrl = $"{baseUrl}ProfilePic";
 
         public static string clientBaseUrl = "http://test.noorcare.net/";
         public static string emailidVerefactionUrl = $"{clientBaseUrl}user/EmailVerfication/CLIENTID";
         public static string logoUrl = $"{clientBaseUrl}assets/images/logos/Logo.png";
-        public static string apiBaseUrl = "http://testapi.noorcare.net/";
-        public static string imgUrl = $"{apiBaseUrl}ProfilePic";
+        public static string baseUrl = "http://testapi.noorcare.net/";
+        public static string imgUrl = $"{baseUrl}ProfilePic";
     }
 }
