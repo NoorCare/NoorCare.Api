7018d4a51f83b3386d978462d9c851c3dddc15c7
addongroup <veerendrasingh34@gmail.com>
2020-08-01 22:03:58 +0530

    constant
    constant

--- WebAPI/constant.cs
+++ WebAPI/constant.cs
@@ -1,18 +1,17 @@
 ﻿namespace WebAPI
 {
     public static class constant
-    {
-         
+    {         
         //public static string emailidVerefactionUrl = "http://pranadafoundation.com/user/EmailVerfication/CLIENTID";
         //public static string logoUrl = "http://pranadafoundation.com/assets/images/logos/Logo.png";
         //public static string imgUrl = "http://API.pranadafoundation.com/";
         //public static string baseUrl = "http://pranadafoundation.com/";
 
         //----------------------
-        public static string emailidVerefactionUrl = "http://noorcare.net/user/EmailVerfication/CLIENTID";
-        public static string logoUrl = "http://noorcare.net/assets/images/logos/Logo.png";
-        public static string imgUrl = "http://API.noorcare.net/";
-        public static string baseUrl = "http://noorcare.net/";
+        public static string emailidVerefactionUrl = "https://noorcare.net/user/EmailVerfication/CLIENTID";
+        public static string logoUrl = "https://noorcare.net/assets/images/logos/Logo.png";
+        public static string imgUrl = "https://API.noorcare.net/";
+        public static string baseUrl = "https://noorcare.net/";
 
         //for Live
 
